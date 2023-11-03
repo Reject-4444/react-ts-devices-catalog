69550bbb14b15f8a35ef5304fe246478b7f5052f
Aleksandr <kartanovaleks@gmail.com>
2023-11-03 10:12:06 +0200

    empty

--- src/helpers/Url.jsx
+++
@@ -1,5 +0,0 @@
-export const URL_PRODUCTS
-= 'https://mate-academy.github.io/react_phone-catalog/api/products.json';
-
-export const URL_PRODUCT_DETAILS = 'https://mate-academy.github.io/'
-+ 'react_phone-catalog/api/products/';
